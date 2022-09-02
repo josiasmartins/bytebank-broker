7fca8175cf039c47d6937d7da68c7e2f5c1c4eb4
Caitano <josiasmartinscaitano@gmail.com>
2022-09-02 17:22:09 -0300

    feat: operador map

--- src/app/acoes/acoes.service.ts
+++ src/app/acoes/acoes.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+import { Acao } from './modelo/acoes';
 
 const API = "http://localhost:3000/acoes"
 
@@ -12,7 +14,32 @@ export class AcoesService {
     private httpClient: HttpClient
   ) { }
 
+  /**
+   *  Promise 
+   * 
+   * @description
+   * Toda operação que pode ser feita com promises, pode também ser feita com observables
+   * 
+   * Com observable é possível representar um único elemento ou uma coleção de 
+   * elementos, enquanto promises representam apenas um elemento.
+   * 
+   * O observable é um tipo de objeto que representa um fluxo de 
+   * informações no tempo, como, por exemplo, os eventos de digitação de um campo.
+   */
   public getAcoes() {
-    return this.httpClient.get<any>(API);
+    return this.httpClient.get<any>(API)
+      .pipe(map((acoes) => acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))));
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+
+    if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    }
+
+    return 0;
   }
 }

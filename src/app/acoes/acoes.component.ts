f524695c0f530d6e230710220bcf95f76a455707
Caitano <josiasmartinscaitano@gmail.com>
2022-09-03 12:22:41 -0300

    feat: usado o método debounceTime

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -3,7 +3,9 @@ import { FormControl } from '@angular/forms';
 import { Acoes } from './modelo/acoes';
 import { AcoesService } from './acoes.service';
 import { merge, Subscription } from 'rxjs';
-import { filter, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, filter, switchMap, tap } from 'rxjs/operators';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -15,6 +17,7 @@ export class AcoesComponent {
   public acoesInput = new FormControl();
   public todaAcoes$ = this.acoesService.getAcoes().pipe(tap(() => console.log('fluxo inicial')), tap(console.log));
   public filtraPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
     tap(() => console.log('fluxo filtro')),
     tap(console.log),
     filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
